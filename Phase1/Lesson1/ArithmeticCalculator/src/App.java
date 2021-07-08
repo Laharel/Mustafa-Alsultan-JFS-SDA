df5f426b3c1624100c38248f6d5081f48a0eed9b
Laharel <mustafas1990@gmail.com>
2021-07-08 04:43:23 +0300

    a lil fix

--- Phase1/Lesson1/ArithmeticCalculator/src/App.java
+++ Phase1/Lesson1/ArithmeticCalculator/src/App.java
@@ -1,40 +1,40 @@
 import java.util.*;
 public class App {
     public static void main(String[] args) {
-        Scanner input= new Scanner(System.in);
+        Scanner scan= new Scanner(System.in);
         System.out.println("Hello, user!");
         System.out.println("This Calculator works by FirstNumber Operator SecondNumber");
         System.out.println("Select operator 0-exit 1-add 2-subtract 3-multiply 4-divide");
-        int operator=input.nextInt();
-        int a,b,result;
+        int operator=scan.nextInt();
+        int ina,inb,result;
         while (operator!=0) {
             if (operator>0 && operator<5){
                 System.out.println("Enter first number- ");  
-                a= input.nextInt();  
+                ina= scan.nextInt();  
                 
                 System.out.println("Enter second number- ");  
-                b= input.nextInt();
+                inb= scan.nextInt();
     
                 if (operator==1) {
-                    result=a+b;
+                    result=ina+inb;
                     System.out.println("Addtion result: "+result);
                 }
                 
                 else if (operator==2)
                 {
-                    result=a-b;
+                    result=ina-inb;
                     System.out.println("Subtraction result: "+result);
                 }
                 
                 else if (operator==3)
                 {
-                    result=a*b;
+                    result=ina*inb;
                     System.out.println("Multiplication result: "+result);
                 }
                 
                 else
                 {
-                    result=a/b;
+                    result=ina/inb;
                     System.out.println("Division result: "+result);
                 }
             }
@@ -44,9 +44,9 @@ public class App {
             }
             
             System.out.println("Select operator 0-exit 1-add 2-subtract 3-multiply 4-divide");
-            operator=input.nextInt();
+            operator=scan.nextInt();
         }
-        input.close();
+        scan.close();
         System.out.println("Bye");
     }
 }
